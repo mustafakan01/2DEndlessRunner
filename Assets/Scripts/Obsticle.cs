c1d6d6e906465f9be927921d546e21b2cda5abf3
Mustafa Kan <mustafakan242@gmail.com>
2024-06-20 15:13:01 +0300

    Game

--- Assets/Scripts/Obsticle.cs
+++ Assets/Scripts/Obsticle.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Obsticle : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject player;
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -16,6 +16,11 @@ public class Obsticle : MonoBehaviour
         if (collision.tag=="Border")
         {
             Destroy(this.gameObject);
+        }
+
+        if (collision.tag == "Player")
+        {
+            Destroy(player.gameObject);
         }
     }
 }
